5b129859c207d814eab478c5f0ba099ef489bd68
epimarti <pierre.martin.epitech@gmail.com>
2017-11-08 23:53:08 +0100

    refactoring fizzbuzz.go

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -5,24 +5,21 @@ import (
   "strconv"
 )
 
+func convert(raw, int1, int2 int, s1, s2 string) string {
+  if raw % (int1 * int2) == 0 { return strings.Join([]string{s1, s2}, " ") }
+
+  if raw % int1 == 0 { return s1 }
+
+  if raw % int2 == 0 { return s2 }
+
+  return strconv.Itoa(raw)
+}
+
 func Generate(int1 int, int2 int, limit int, s1 string, s2 string) []string {
   result := make([]string, limit)
 
   for i := 0; i < limit; i++ {
-    current := i + 1
-    var sb []string
-    if current % int1 == 0 {
-      sb = append(sb, s1)
-    }
-    if current % int2 == 0 {
-      sb = append(sb, s2)
-    }
-
-    if len(sb) > 0 {
-      result[i] = strings.Join(sb, " ")
-    } else {
-      result[i] = strconv.Itoa(current)
-    }
+    result[i] = convert(i + 1, int1, int2, s1, s2)
   }
 
   return result
